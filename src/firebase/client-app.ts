371ce1d739166364b3242330870cb3a3f0b20cd6
Pedro Tescaro <pedroatescaro@gmail.com>
2025-08-19 21:30:42 +0000

    usa npm install firebase // Import the functions you need from the SDKs

--- src/firebase/client-app.ts
+++ src/firebase/client-app.ts
@@ -2,12 +2,12 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDX0mJJt5SW2L55Fs5SPWHsXP2gQHFbRPY",
+  authDomain: "woundwise-g3zb9.firebaseapp.com",
+  projectId: "woundwise-g3zb9",
+  storageBucket: "woundwise-g3zb9.firebasestorage.app",
+  messagingSenderId: "315167035013",
+  appId: "1:315167035013:web:c6b876cc9a961e0ff963ec"
 };
 
 // Initialize Firebase
