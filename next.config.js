f42b0078e8b6977e1269a4573ca12f60eec45c45
Pedro Tescaro <pedroatescaro@gmail.com>
2025-09-26 22:21:33 +0000

    [fix] linha do dev no package

---
+++ next.config.js
@@ -0,0 +1,26 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Otimização de Imagens
+  images: {
+    // Formatos de imagem modernos para melhor compressão e qualidade
+    formats: ['image/avif', 'image/webp'],
+    // Domínios remotos permitidos para imagens (se houver)
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'example.com',
+    //     port: '',
+    //     pathname: '/images/**',
+    //   },
+    // ],
+  },
+
+  // Otimizações de Compilador
+  compiler: {
+    // Remove `console.*` em produção para um build mais limpo
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+};
+
+module.exports = nextConfig;
